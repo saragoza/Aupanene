82c2974c66017a4bc0bd3026d7b08edda542a497
saragoza <ssantander3@yahoo.es>
2016-05-03 00:15:17 +0200

    Actualización de código, optimización de imágenes

--- js/aupanene-galeria.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $(".thumbnail").click(function(evento){
-        evento.preventDefault();
-        $("#imagen-ampliada img").attr("src", $(this).children("img").attr("src"));
-        });
-});
